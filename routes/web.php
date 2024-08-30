62173662bffd8a4791017a28048e9124564baac2
YuliaGolovina <y.golovina@tg.dunice.net>
2024-08-30 15:22:20 +0300

    create app

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/user/{id}', [UserController::class, 'show']);
