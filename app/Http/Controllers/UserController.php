4ae3b9abb409eb543a217a97be9e7a59ffe50b41
YuliaGolovina <y.golovina@tg.dunice.net>
2024-08-30 18:08:40 +0300

    add UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Resources\UserResource;
 
-use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\User;
 
-class UserController extends Controller
+class UserController
 {
-    //
+    public function show($id) {
+        try {
+            $userData = User::with('posts.user')->findOrFail($id);
+            return (new UserResource($userData))->response()->setStatusCode(200, 'OK');
+        }
+        catch(ModelNotFoundException $e)
+        {
+            report($e);
+            return response('Not found ID')->setStatusCode(204);
+        }
+    }
 }
